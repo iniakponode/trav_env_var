21678fa6efae32ea2c2721ba40f74753543fc230
iniakponode <iniakpothompson.coins@gmail.com>
2022-07-07 12:45:13 +0100

    Travis file update

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
-
+docker_username=process.env.DOCKER_USERNAME;
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 async function main() {
   while(true) {
-    docker_username=process.env.DOCKER_USERNAME;
     console.log("Environment variables ${docker_username}");
     await sleep(5000);
   }
